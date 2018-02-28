236b96f4dfeee90636dfd4efaf9b471e0072aa9b
angelacastanieto <angela@gametime.co>
2018-02-27 23:59:08 -0800

    stuff works but cookies nt persisting

--- handlers.go
+++ handlers.go
@@ -1,60 +1,71 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/markbates/goth/gothic"
 )
 
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	keys, ok := r.URL.Query()["id"]
+func GetUser(c echo.Context) error {
+	id := c.Param("id")
+	fmt.Println("ID", id)
 
-	if !ok || len(keys) < 1 {
-		log.Println("Url Param 'id' is missing")
-		return
+	sess, err := session.Get("sessions", c)
+	if err != nil {
+		fmt.Println(err)
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"errors": []string{err.Error()}})
 	}
 
-	id := keys[0]
-
-	session, _ := store.Get(r, id)
-
 	// Retrieve our access_token and type-assert it
-	token, ok := session.Values["access_token"].(string)
+	token, ok := sess.Values["access_token"].(string)
 
 	if !ok {
-		return
+		return c.JSON(http.StatusNotFound, map[string]interface{}{"errors": []string{"token not found"}})
 	}
 
 	activities, err := fitbitClient.Activities(id, time.Now().Format("2006-01-02"), token)
 	if err != nil {
-		return
+		fmt.Println(err)
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{"errors": []string{err.Error()}})
 	}
 
 	spew.Dump(activities)
-	json.NewEncoder(w).Encode(r)
+	return c.JSON(http.StatusOK, map[string]interface{}{"activities": activities})
 }
 
-func CallbackHandler(w http.ResponseWriter, r *http.Request) {
-	user, err := gothic.CompleteUserAuth(w, r)
+func AuthHandler(c echo.Context) error {
+	gothic.BeginAuthHandler(c.Response(), c.Request())
 
+	return nil
+}
+
+func CallbackHandler(c echo.Context) error {
+	user, err := gothic.CompleteUserAuth(c.Response(), c.Request())
 	if err != nil {
-		fmt.Fprintln(w, err)
-		return
+		fmt.Println(err)
+		http.Redirect(c.Response().Writer, c.Request(), "http://localhost:3000", http.StatusInternalServerError)
 	}
 
-	session, _ := store.Get(r, user.UserID)
+	sess, err := session.Get("sessions", c)
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(c.Response().Writer, c.Request(), "http://localhost:3000", http.StatusInternalServerError)
+	}
 
-	session.Values["access_token"] = user.AccessToken
-	session.Values["refresh_token"] = user.RefreshToken
+	sess.Values["access_token"] = user.AccessToken
+	sess.Values["refresh_token"] = user.RefreshToken
 
-	// Save it before we write to the response/return from the handler.
-	session.Save(r, w)
+	err = sess.Save(c.Request(), c.Response())
+	fmt.Println("ERRR IS", err)
+	if err != nil {
+		fmt.Println(err)
+		http.Redirect(c.Response().Writer, c.Request(), "http://localhost:3000", http.StatusInternalServerError)
+	}
 
-	spew.Dump(user) // Store USER KEYS HERE
-	http.Redirect(w, r, "http://localhost:3000/users/"+user.UserID, http.StatusSeeOther)
+	return c.Redirect(http.StatusTemporaryRedirect, "http://localhost:3000/users/"+user.UserID)
 }
--- server.go
+++ server.go
@@ -4,11 +4,11 @@ import (
 	"os"
 
 	"github.com/angelacastanieto/hioqi/fitbitclient"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/pat"
 	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/middleware"
 	"github.com/markbates/goth"
-	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/fitbit"
 )
 
@@ -20,6 +20,20 @@ var (
 var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_SECRET")))
 
 func init() {
+	store.Options = &sessions.Options{
+		Domain:   "localhost",
+		Path:     "/",
+		MaxAge:   3600 * 8, // 8 hours
+		HttpOnly: true,
+	}
+}
+
+func main() {
+	fitbitClient, err = fitbitclient.NewAPI()
+	if err != nil {
+		panic(err)
+	}
+
 	goth.UseProviders(
 		fitbit.New(
 			os.Getenv("FITBIT_KEY"),
@@ -31,21 +45,31 @@ func init() {
 			fitbit.ScopeNutrition,
 		),
 	)
+
+	e := echo.New()
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
+	e.Use(fitbitAuth())
+	// CORS middleware
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	}))
+
+	e.GET("/auth/:provider/callback", CallbackHandler)
+	e.GET("/auth/:provider", AuthHandler)
+	e.GET("/users/:id", GetUser)
+
+	e.Logger.Fatal(e.Start(":8000"))
 }
 
-func main() {
-	fitbitClient, err = fitbitclient.NewAPI()
-	if err != nil {
-		panic(err)
+// middleware to build fitbit oauth urls
+func fitbitAuth() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			values := c.Request().URL.Query()
+			values.Add("provider", "fitbit")
+			c.Request().URL.RawQuery = values.Encode()
+			return next(c)
+		}
 	}
-
-	p := pat.New()
-
-	p.Get("/auth/{provider}/callback", CallbackHandler)
-	p.Get("/auth/{provider}", gothic.BeginAuthHandler)
-	p.Get("/users/{id}", GetUser)
-
-	n := negroni.Classic()
-	n.UseHandler(p)
-	n.Run(":8000")
 }
